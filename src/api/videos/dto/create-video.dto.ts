1d756e854125dc49a447a02c1183bc680ea66afc
grishaharutyunyan <grishaharutyunian@gmail.com>
2026-06-05 17:49:27 +0400

    update CreateVideoDto with validation and add uploads to .gitignore

--- src/api/videos/dto/create-video.dto.ts
+++ src/api/videos/dto/create-video.dto.ts
@@ -1,9 +1,19 @@
-import { LocaleText } from '../../events/event-i18n.types';
+import { IsNumber, IsObject, IsOptional, IsString } from 'class-validator';
+import type { LocaleText } from '../../events/event-i18n.types';
 
 export class CreateVideoDto {
+  @IsObject()
   title: LocaleText;
+
+  @IsObject()
   subtitle: LocaleText;
+
+  @IsString()
   url: string;
+
+  @IsOptional() @IsString()
   thumbnailUrl?: string | null;
+
+  @IsOptional() @IsNumber()
   priority?: number;
 }
